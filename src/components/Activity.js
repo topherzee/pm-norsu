8d49c8651906f77c986a277982c588ddcad296b2
czimmermann <christopher.zimmermann@magnolia-cms.com>
2023-05-04 13:47:26 +0200

    referenceDepth:4

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Activity = (props) => (
   <>
-    <h3>Activity: {props.activity.name}</h3>
+    <h3>Activity: {props.targetActivity.name}</h3>
     <p>{props.text || "-"}</p>
     <img
       className="Image"
-      src={props.activity.image["@link"] + "?width=120&height=90"}
+      src={props.targetActivity.image["@link"] + "?width=120&height=90"}
       alt="Etiam Purus"
     />
   </>
